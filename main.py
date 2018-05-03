d12404c7863291f6a82640e2bf522f64c28c6b85
Jakub Tomczak <jakub.tomczak@live.com>
2018-05-03 18:34:00 +0200

    Rename tensorflow dir, add scripts to run project with command line config.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import sys
 from pandas import read_csv
 import os.path
 from shutil import copyfile
 
+import api.retrain as retrain
+import tensorflow as tf
 from tools.RecoverableError import RecoverableError
 
+current_dir = '{}'.format( os.getcwd() )
+csv_filename = 'decor.csv'
+categories_dir = 'patterns'
+categories = ['country', 'decor', 'type']
 
 def read_csv_data(file):
     if not os.path.isfile(file):
@@ -30,22 +37,22 @@ def create_categories_directories(dataset):
 def put_images_in_proper_category_dirs(dataset):
     for data_row in dataset.iterrows():
         for category in categories:
-            copyfile(os.path.join(current_dir, 'resources', 'decor', data_row[1].file),
-                     os.path.join(current_dir, categories_dir, get_nonspace_category_name(data_row[1][category]), data_row[1].file))
+            file = data_row[1].file.replace('png', 'jpg')
+            copyfile(os.path.join(current_dir, 'resources', 'decor', file),
+                     os.path.join(current_dir, categories_dir, get_nonspace_category_name(data_row[1][category]), file))
 
-current_dir = '{}'.format( os.getcwd() )
-csv_filename = 'decor.csv'
-categories_dir = 'patterns'
-categories = ['country', 'decor', 'type']
-
-def main():
+def prepare_dataset():
     path = os.path.join(current_dir, 'resources', csv_filename)
     dataset = read_csv_data(path)
     create_categories_directories(dataset)
     put_images_in_proper_category_dirs(dataset)
 
+
 if __name__ == "__main__" :
+    FLAGS, unparsed = retrain.runparser()
+
     try:
-        main()
+        prepare_dataset()
+        tf.app.run(main=retrain.main, argv=[sys.argv[0]] + unparsed)
     except RecoverableError as e:
         print(e)
