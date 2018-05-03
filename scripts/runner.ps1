57fe7e83827782a962da773d3f78f386dd963030
Jakub Tomczak <jakub.tomczak@live.com>
2018-05-03 18:46:46 +0200

    Change tensorflow path to api.

--- scripts/runner.ps1
+++ scripts/runner.ps1
@@ -1,3 +1,3 @@
-$env:IMAGE_SIZE=224
+$env:IMAGE_SIZE=160
 $env:ARCHITECTURE="mobilenet_0.50_$env:IMAGE_SIZE"
-python ..\\tensorflow\\retrain.py --bottleneck_dir=..\\output\\bottlenecks --how_many_training_steps=1000 --model_dir=..\\tensorflow\\models\\ --summaries_dir=..\\output\\pattern_training_summaries\\"$env:ARCHITECTURE"  --output_graph=..\\output\\retrained_graph_patterns.pb  --output_labels=..\\output\\retrained_labels.txt  --architecture="$env:ARCHITECTURE" --image_dir=..\\patterns
+python ..\\api\\retrain.py --bottleneck_dir=..\\output\\bottlenecks --how_many_training_steps=1000 --model_dir=..\\api\\models\\ --summaries_dir=..\\output\\pattern_training_summaries\\"$env:ARCHITECTURE"  --output_graph=..\\output\\retrained_graph_patterns.pb  --output_labels=..\\output\\retrained_labels.txt  --architecture="$env:ARCHITECTURE" --image_dir=..\\patterns
--- scripts/test_image.ps1
+++ scripts/test_image.ps1
@@ -1,3 +1,3 @@
 param([string]$filename)
 echo "Trying to classify an image $filename"
-python ..\\tensorflow\\label_image.py --graph=..\\output\\retrained_graph_patterns.pb --labels=..\\output\\retrained_labels.txt --image=..\\resources\\decor\\$filename
+python ..\\api\\label_image.py --graph=..\\output\\retrained_graph_patterns.pb --labels=..\\output\\retrained_labels.txt --image=..\\resources\\decor\\$filename
