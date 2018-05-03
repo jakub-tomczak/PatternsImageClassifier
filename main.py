a6b725de00a0baf656dba0626c369ddb9307a0ee
Jakub Tomczak <jakub.tomczak@live.com>
2018-05-03 14:41:50 +0200

    Fix categories with space in a name.

--- main.py
+++ main.py
@@ -13,27 +13,30 @@ def read_csv_data(file):
 def make_categories_dir():
     os.makedirs(os.path.join(current_dir, categories_dir))
 
+def get_nonspace_category_name(category):
+    return category.replace(' ', '_')
+
 def create_categories_directories(dataset):
     if not os.path.exists(categories_dir):
         make_categories_dir()
 
-    categories = ['country', 'decor']
     for category in categories:
         for country in dataset[category].unique():
+            country = get_nonspace_category_name(country)
             path_to_create = os.path.join(current_dir, categories_dir, country)
             if not os.path.exists(path_to_create):
                 os.makedirs(path_to_create)
 
 def put_images_in_proper_category_dirs(dataset):
-    categories = ['country', 'decor']
     for data_row in dataset.iterrows():
         for category in categories:
             copyfile(os.path.join(current_dir, 'resources', 'decor', data_row[1].file),
-                     os.path.join(current_dir, categories_dir, data_row[1][category], data_row[1].file))
+                     os.path.join(current_dir, categories_dir, get_nonspace_category_name(data_row[1][category]), data_row[1].file))
 
 current_dir = '{}'.format( os.getcwd() )
 csv_filename = 'decor.csv'
 categories_dir = 'patterns'
+categories = ['country', 'decor', 'type']
 
 def main():
     path = os.path.join(current_dir, 'resources', csv_filename)
